97119fec862a51f2acf3b025614598a9f972a1dc
kezunren <kezunren@itouchtv.cn>
2021-04-26 14:34:37 +0800

    update: add/edit files

--- 函数/函数扁平化/compose.js
+++ 函数/函数扁平化/compose.js
@@ -1,5 +1,6 @@
 // 函数扁平化
 // 思想就是上一个函数的返回值作为下一个函数的参数
+// 思想是compose接受不同个数的参数，每次返回都是一个函数，参数个数可以为0个、1个、多个
 function compose(...fn) {
     if (fn.length == 0) {
         return args => args;
@@ -30,6 +31,6 @@ function mid3(next) {
     return next;
 }
 
-console.log(compose()()); // 参数为空时
-console.log(compose(mid1)(6)); // 参数为一个函数 
-console.log(compose(mid1, mid2, mid3)(5)); 
+// console.log("null params", compose()()); // 参数为空时
+// console.log("one params", compose(mid1)(6)); // 参数为一个函数 
+console.log("more params", compose(mid1, mid2, mid3)(5)); 
